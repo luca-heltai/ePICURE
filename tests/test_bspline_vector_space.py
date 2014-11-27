e061d4e75f365cc681aa63fb1f546da525f5fdb5
Marco Tezzele <marcotez@gmail.com>
2014-11-27 22:07:44 +0100

    added test bspline file

---
+++ tests/test_bspline_vector_space.py
@@ -0,0 +1,19 @@
+from interfaces.vector_space import *
+
+def test_vector_space_interface():
+    a = VectorSpace()
+    assert a.n_dofs == 1
+    assert a.n_cells == 1
+    try:
+        a.check_index(2)
+        assert False, 'Expecting Failure!'
+    except:
+        pass
+    assert a.basis(0)(.5) == 1
+    
+    try:
+        a.basis(0)(1.2)
+        assert False, 'Expecting Failure!'
+    except:
+        pass
+    
