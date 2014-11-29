af02567edf0550fd69d03d4d77e6ab3ffd531aca
Marco Tezzele <marcotez@gmail.com>
2014-11-29 14:31:52 +0100

    base version of the constructor and test running properly

--- interfaces/bspline_vector_space.py
+++ interfaces/bspline_vector_space.py
@@ -1,6 +1,15 @@
 import numpy as np
+# import Nurbs as nurb
 
-class VectorSpace:
+# cntrl = [[-50., -75., 25., 0., -25., 75., 50.],
+#          [25., 50., 50., 0., -50., -50., 25.]]
+# knots = [0., 0., 0., .2, .4, .6, .8, 1., 1., 1.]
+
+# crv = nurb.Crv(cntrl, knots)
+# crv.plot()
+
+
+class BsplineVectorSpace(object):
     """An abstract python interface used to describe *one dimensional
     functions* on `[a,b]`, as *coefficients* times *basis functions*, with
     access to single basis functions, their derivatives, their support and
@@ -9,46 +18,56 @@ class VectorSpace:
 
     The base class constructs the constant vector space.
     """
-    def __init__(self, a=0.0, b=1.0):
+    def __init__(self, degree=0, knots=[0., 1.]):
         """ Pure interface class. It generates the constant on [0,1]. """
-        self.n_dofs = 1
-        self.n_dofs_per_end_point = 0
-        self.n_cells = 1
-        self.cells = np.array([a, b])
+        assert degree >= 0
+        assert len(knots) > 0
+        self.degree = degree
+        self.knots = np.asarray(knots, np.float)
+        self.knots_unique = np.unique(self.knots)
 
-    def check_index(self, i):
-        assert i< self.n_dofs, \
-            'Trying to access base %, but we only have %'.format(i, self.n_dofs)
+        self.n_knots = self.knots.shape[0]
+ 
+        # self.n_dofs = 1
+        # self.n_dofs_per_end_point = 0
+        # self.n_cells = 1
+        # self.cells = np.array([a, b])
 
-    def basis(self, i):
-        self.check_index(i)
-        return lambda x: 1.0
+    # def check_index(self, i):
+    #     assert i< self.n_dofs, \
+    #         'Trying to access base %, but we only have %'.format(i, self.n_dofs)
 
-    def basis_der(self, i, d):
-        self.check_index(i)
-        return lambda x: 0.0
+    # def basis(self, i):
+    #     self.check_index(i)
+    #     return lambda x: 1.0
 
-    def basis_span(self, i):
-        """ VectorSpace.basis_span(i): a tuple indicating the start and end indices into the cells object where the i-th basis function is different from zero"""
-        self.check_index(i)
-        return (0, 1)
+    # def basis_der(self, i, d):
+    #     self.check_index(i)
+    #     return lambda x: 0.0
 
-    def cell_span(self, i):
-        """ An array of indices containing the basis functions which are non zero on the i-th cell """
-        self.check_index(i)
-        return [0]
+    # def basis_span(self, i):
+    #     """ VectorSpace.basis_span(i): a tuple indicating the start and end indices into the cells object 
+    #     where the i-th basis function is different from zero"""
+    #     self.check_index(i)
+    #     return (0, 1)
 
-    def eval(self, c, x):
-        assert len(c) == self.n_dofs, \
-            'Incompatible vector. It should have length %. It has lenght %'.format(self.n_dofs, len(c))
-        # Find the cell where x lies:
-        y = 0*x
-        # TBD: make this function aware of locality
-        for i in xrange(self.n_dofs):
-            y += self.basis(i)(x)*c[i]
+    # def cell_span(self, i):
+    #     """ An array of indices containing the basis functions which are non zero on the i-th cell """
+    #     self.check_index(i)
+    #     return [0]
+
+    # def eval(self, c, x):
+    #     assert len(c) == self.n_dofs, \
+    #         'Incompatible vector. It should have length %. It has lenght %'.format(self.n_dofs, len(c))
+    #     # Find the cell where x lies:
+    #     y = 0*x
+    #     # TBD: make this function aware of locality
+    #     for i in xrange(self.n_dofs):
+    #         y += self.basis(i)(x)*c[i]
     
-    def element(self, c):
-        """  VectorSpace.element(c): a callable function, representing sum(c[i] * basis[i]), which exploits the locality of the basis functions """
-        assert len(c) == self.n_dofs, \
-            'Incompatible vector. It should have length %. It has lenght %'.format(self.n_dofs, len(c))
-        return lambda x: self.eval(c, x)
+    # def element(self, c):
+    #     """  VectorSpace.element(c): a callable function, representing sum(c[i] * basis[i]), which exploits 
+    #     the locality of the basis functions """
+    #     assert len(c) == self.n_dofs, \
+    #         'Incompatible vector. It should have length %. It has lenght %'.format(self.n_dofs, len(c))
+    #     return lambda x: self.eval(c, x)
--- tests/test_bspline_vector_space.py
+++ tests/test_bspline_vector_space.py
@@ -1,19 +1,42 @@
-from interfaces.vector_space import *
+from interfaces.bspline_vector_space import *
+from nose.tools import *
 
-def test_vector_space_interface():
-    a = VectorSpace()
-    assert a.n_dofs == 1
-    assert a.n_cells == 1
-    try:
-        a.check_index(2)
-        assert False, 'Expecting Failure!'
-    except:
-        pass
-    assert a.basis(0)(.5) == 1
+def test_bspline_vector_space_default_constructor():
+    a = BsplineVectorSpace()
+    assert a.degree == 0
+    assert a.knots[0] == 0.
+    assert a.knots[1] == 1.
+    assert a.knots_unique[0] == 0.
+    assert a.knots_unique[0] == 0.
+    assert a.n_knots == 2
     
-    try:
-        a.basis(0)(1.2)
-        assert False, 'Expecting Failure!'
-    except:
-        pass
+
+def test_bspline_vector_space_general_constructor():
+    a = BsplineVectorSpace(2, [0.,0.,0.,.5,1.,1.,1.])
+    assert a.degree == 2
+    assert a.knots[0] == 0.
+    assert a.knots[1] == 0.
+    assert a.knots[2] == 0.
+    assert a.knots[3] == 0.5
+    assert a.knots[4] == 1.
+    assert a.knots[5] == 1.
+    assert a.knots[6] == 1.
+    
+    assert a.knots_unique[0] == 0.
+    assert a.knots_unique[1] == 0.5
+    assert a.knots_unique[2] == 1.
+    assert a.n_knots == 7
+
+    # try:
+    #     a.check_index(2)
+    #     assert False, 'Expecting Failure!'
+    # except:
+    #     pass
+    # assert a.basis(0)(.5) == 1
+    
+    # try:
+    #     a.basis(0)(1.2)
+    #     assert False, 'Expecting Failure!'
+    # except:
+    #     pass
     
