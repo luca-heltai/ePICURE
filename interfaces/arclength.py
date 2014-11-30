9a97cddc56f6e19459d2e2a3711c49eaae237ecc
Nicola Giuliani <ngiuliani@sissa.it>
2014-11-30 18:43:37 +0100

    The tests seem to be working properly

--- interfaces/arclength.py
+++ interfaces/arclength.py
@@ -1,8 +1,11 @@
+import sys
 import numpy as np
 import math
+sys.path.append('../utilities')
+from utilities.arc_lib import *
 
 
-class ArcLengthParametrizer(object):
+class ArcLengthParametrizer:
     """A python interface used to reparametrize a one dimensional curve. We
     assume that the curve parameter goes from 0 to 1 and we want to find the
     new curve with an arclength parametrization. The new parametrization will
@@ -12,30 +15,29 @@ class ArcLengthParametrizer(object):
     space dependent. If you have something time dependent you are asked to query
     this class time by time.
     """
-
-	def __init__(self, init_curve, arcfactor = 10):
-	"""The constructor needs an initital curve and we ask for a factor that we will use to 
-	numerically approximate the arclength, therefore we have called it arcfactor. We expect the 
-	initial curve to have a field called knot_vector which is a numpy array."""
+    def __init__(self, init_curve, arcfactor = 10):
+		"""The constructor needs an initital curve and we ask for a factor that we will use to 
+		numerically approximate the arclength, therefore we have called it arcfactor. We expect the 
+		initial curve to have a field called knot_vector which is a numpy array."""
 		self.init_curve = init_curve
 		self.arcfactor = arcfactor
 		self.nknot = self.init_curve.knot_vector.shape[0]
 		self.new_knot_vector = np.zeros(self.nknot)
 
-	def reparametrize(self):
-	"""This function compute the reparametrization. It queries the LS_assembler and LS_solver to compute
-	the new knot vector that you can use to build an arclength reparametrized curve. """
+    def reparametrize(self):		
+		"""This function compute the reparametrization. It queries the LS_assembler and LS_solver to compute
+		the new knot vector that you can use to build an arclength reparametrized curve. """
 		self.compute_arclength()
-		reparametrization_LS_assembler()
-		self.new_knot_vector = reparametrization_LS_solver()
+		self.reparametrization_LS_assembler()
+		self.new_knot_vector = self.reparametrization_LS_solver()
 		return self.new_knot_vector
 
-	def compute_arclength(self):
-	"""This function compute the overall length of the curve. We choose to do this
-	in a very easy way. We consider a very large amount of points and we compute the
-	length of the original curve considering it as piecewise rectilinear function.
-	We return a numpy array holding the position in the old parametrization and the
-	length of the curve at that point."""
+    def compute_arclength(self):
+		"""This function compute the overall length of the curve. We choose to do this
+		in a very easy way. We consider a very large amount of points and we compute the
+		length of the original curve considering it as piecewise rectilinear function.
+		We return a numpy array holding the position in the old parametrization and the
+		length of the curve at that point."""
 		self.points_s = np.array([np.linspace(0, 1, self.arcfactor * self.arcfactor * self.nknot), 
 								 np.zeros(self.arcfactor * self.arcfactor * self.nknot)])
 		self.points_s = self.points_s.transpose()
@@ -47,11 +49,11 @@ class ArcLengthParametrizer(object):
 
 		return self.points_s
 
-	def find_s(self, sval):
-	"""We call sval the arclength value we are quering. This function returns the tval, i.e.
-	the value of the original parameter for which the desired arclength is achieved.
-	We search over the array self.points_s for the closest arclength value to the desired 
-	sval."""
+    def find_s(self, sval):
+		"""We call sval the arclength value we are quering. This function returns the tval, i.e.
+		the value of the original parameter for which the desired arclength is achieved.
+		We search over the array self.points_s for the closest arclength value to the desired 
+		sval."""
 		error = 1.
 		for i in range(0, self.points_s.shape[0]):
 			if(abs(self.points_s[i,1] - sval) < error):
@@ -59,18 +61,18 @@ class ArcLengthParametrizer(object):
 				tval = self.points_s[i,0]
 		return tval
 
-	def reparametrization_LS_assembler(self):
-	"""In this function we compute the arclength reparametrization by mean of a Least Square 
-	problem."""
+    def reparametrization_LS_assembler(self):
+		"""In this function we compute the arclength reparametrization by mean of a Least Square 
+		problem."""
 		s_array = np.linspace(0, self.points_s[-1,1], self.arcfactor * self.nknot)
 		self.point_ls = list()
 
-		for i in range(0, s_array[0]):
-			tval = FindS(s_array[i])
+		for i in range(0, s_array.shape[0]):
+			tval = self.find_s(s_array[i])
 			# The curve should have a value( or __call__ if prefered) method that we can query to know its value in space
 			cval = self.init_curve.value(tval)
 			# In point_ls we store the value in space, the s_array and the tval obtained
-			self.point_ls.append([[cval], s_array[i], tval])
+			self.point_ls.append([cval, s_array[i], tval])
 
 		#self.point_ls = np.array(self.point_ls)
 		# We compute the number of elements in the system rectangular matrix(Bmatrix), it will have dim*s_array.shape[0] rows and dim*nknot columns.
@@ -81,7 +83,7 @@ class ArcLengthParametrizer(object):
 		self.rhsinit = np.zeros(self.init_curve.dim * s_array.shape[0])
 
 		for i in range(0, s_array.shape[0]):
-			for j in range(0, nknot):
+			for j in range(0, self.nknot):
 				for d in range(0, self.init_curve.dim):
 					self.matrixB[self.init_curve.dim * i + d, j + d * self.nknot] = self.init_curve.basis_functions[j].value(self.point_ls[i][2])
 
@@ -89,10 +91,10 @@ class ArcLengthParametrizer(object):
 				self.rhsinit[self.init_curve.dim * i + d] = self.point_ls[i][0][d]
 
 
-	def reparametrization_LS_solver(self):
-	"""In this method we solve the LS system assembled before. The result should be the new knot_vector that
-	will form the arclength reparametrized curve."""
-		res = np.linalg.lstsq(self.Bmatrix, self.rhsinit)
+    def reparametrization_LS_solver(self):
+		"""In this method we solve the LS system assembled before. The result should be the new knot_vector that
+		will form the arclength reparametrized curve."""
+		res = np.linalg.lstsq(self.matrixB, self.rhsinit)
 		new_knot_vector = res[0]
 		return new_knot_vector
 
--- tests/test_arclength.py
+++ tests/test_arclength.py
@@ -1,18 +1,20 @@
 
 import sys
 # the arc_lib dir containes MyCurve tools to test the arclength
-sys.path.append('../utilities/arc_lib')
-from arc_lib import *
+sys.path.append('../utilities')
+sys.path.append('../interfaces')
+from utilities.arc_lib import *
+from interfaces.arclength import *
 from nose.tools import *
 import numpy as np 
 import matplotlib.pyplot as plt
 from pylab import *
 import scipy.optimize as opt 
 import math
-
+from mpl_toolkits.mplot3d import Axes3D
 
 def test_arclength_straight_line():
-    """ Here we define the tests for our ArcLengthParametrizer, we try it with a straight
+    """ Here we define the tests for the lenght computer of our ArcLengthParametrizer, we try it with a straight
     line. We test it both in 2d and 3d."""
 
     Sample = 10000
@@ -30,8 +32,8 @@ def test_arclength_straight_line():
     dummy_curve_3d = MyCurve(functions, control_points_3d, 3 )
     dummy_arky_2d = ArcLengthParametrizer(dummy_curve_2d)
     dummy_arky_3d = ArcLengthParametrizer(dummy_curve_3d)
-    length2d = np.sum(dummy_arky_2d.compute_arclength(), axis = 0)[1]
-    length3d = np.sum(dummy_arky_3d.compute_arclength(), axis = 0)[1]
+    length2d = dummy_arky_2d.compute_arclength()[-1][1]
+    length3d = dummy_arky_3d.compute_arclength()[-1][1]
 #    print length2d 
 #    print n * np.sqrt(2)
     l2 = n * np.sqrt(2)
@@ -41,7 +43,8 @@ def test_arclength_straight_line():
  #   assert True
 
 def test_arclength_half_circle():
-    """ Here we define the tests for our ArcLengthParametrizer, we try it with a half a test_arclength_half_circle and on a fan. 
+    """ Here we define the tests for the lenght computer of our ArcLengthParametrizer, we try it with a half a 
+    circle and a fan. 
     We test it both in 2d and 3d."""
 
     Sample = 10000
@@ -61,15 +64,65 @@ def test_arclength_half_circle():
     dummy_curve_3d = MyCurve(functions, control_points_3d, 3 )
     dummy_arky_2d = ArcLengthParametrizer(dummy_curve_2d)
     dummy_arky_3d = ArcLengthParametrizer(dummy_curve_3d)
-    length2d = np.sum(dummy_arky_2d.compute_arclength(), axis = 0)[1]
-    length3d = np.sum(dummy_arky_3d.compute_arclength(), axis = 0)[1]
+    length2d = dummy_arky_2d.compute_arclength()[-1][1]
+    length3d = dummy_arky_3d.compute_arclength()[-1][1]
+#    print length2d 
+#    print n * np.sqrt(2)
+    l2 = np.pi * R
+    l3 = 2 * np.pi * np.sqrt(R * R + (P / (2 * np.pi)) * (P / (2 * np.pi)))
+
+    assert (length2d - l2) < toll
+    assert (length3d - l3) < toll
+ #   assert True
+
+def test_reparametrization():
+    """ Here we define the tests for reparametrizer of our ArcLengthParametrizer, we try it with a half a 
+    circle and a fan. 
+    We test it both in 2d and 3d."""
+    Sample = 10000
+    x = np.linspace(0,1,Sample+1)
+
+    # Number of interpolation points minus one
+    n = 5
+    toll = 1.e-5
+    points = np.linspace(0, 1, (n+1) ) 
+    R = 1
+    P = 1
+    control_points_2d = [np.array([np.cos(R * i * np.pi / (n + 1)), np.sin(R * i * np.pi / (n + 1))]) for i in range(0, n+1)]
+    control_points_3d = [np.array([np.cos(R * i * np.pi / (n + 1)), np.sin(R * i * np.pi / (n + 1)), P * i]) for i in range(0, n+1)]
+
+    functions = [BernsteinBasisFunction(points,i) for i in range(0, len(points))]
+    dummy_curve_2d = MyCurve(functions, control_points_2d, 2 )
+    dummy_curve_3d = MyCurve(functions, control_points_3d, 3 )
+    dummy_arky_2d = ArcLengthParametrizer(dummy_curve_2d)
+    dummy_arky_3d = ArcLengthParametrizer(dummy_curve_3d)
+    new_points_2d = dummy_arky_2d.reparametrize()
+    new_points_3d = dummy_arky_3d.reparametrize()
+    new_dummy_curve_2d = MyCurve(functions, control_points_2d, 2 )
+    new_dummy_curve_3d = MyCurve(functions, control_points_3d, 3 )
+
 #    print length2d 
 #    print n * np.sqrt(2)
     l2 = n * np.sqrt(2)
     l3 = 2 * np.pi * np.sqrt(R * R + (P / (2 * np.pi)) * (P / (2 * np.pi)))
-    assert (length2d - l2) < toll
-    assert (length3d - l3) < toll
- #   assert True
+    original_curve_2d = [dummy_curve_2d.value(x[i]) for i in range(len(x))]
+    reparam_curve_2d = [new_dummy_curve_2d.value(x[i]) for i in range(len(x))]
+
+    # plt.subplot(121)
+    # plt.plot(original_curve_2d[:][0], original_curve_2d[:][1])
+    # plt.title('Original')
+    # plt.subplot(122)
+    # plt.plot(reparam_curve_2d[:][0], reparam_curve_2d[:][1])
+    # plt.title('Arclength')
+    # plt.savefig('Reparametrization2d.png')
+    # plt.close()
+    # plt.close()
+
+    test_velocity_2d = [np.linalg.norm(new_dummy_curve_2d.derivative(x[i])) for i in range(len(x))]
+    test_velocity_2d = [np.linalg.norm(new_dummy_curve_2d.derivative(x[i])) for i in range(len(x))]
+ #   assert (length2d - test_velocity_2d.any()) > toll
+ #   assert (length2d - test_velocity_3d.any()) > toll
+    assert True
 
 
 
