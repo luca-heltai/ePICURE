a3906165ba4718277c7f2a646d121b2d0a84ea85
Marco Tezzele <marcotez@gmail.com>
2014-12-01 12:21:14 +0100

    Added tests for compute_n_dofs and compute_mults

--- tests/test_bspline_vector_space.py
+++ tests/test_bspline_vector_space.py
@@ -1,5 +1,6 @@
 from interfaces.bspline_vector_space import *
 from nose.tools import *
+import numpy as np
 
 def test_bspline_vector_space_default_constructor():
     a = BsplineVectorSpace()
@@ -44,16 +45,20 @@ def test_bspline_vector_space_general_constructor():
 
     assert a.n_dofs == 4
 
-    # try:
-    #     a.check_index(2)
-    #     assert False, 'Expecting Failure!'
-    # except:
-    #     pass
-    # assert a.basis(0)(.5) == 1
-    
-    # try:
-    #     a.basis(0)(1.2)
-    #     assert False, 'Expecting Failure!'
-    # except:
-    #     pass
-    
+
+def test_compute_mults():
+    a = BsplineVectorSpace(3, [0.,0.,0.,0.,2.,2.,3.,5.,5.,5.,5.])
+    mults = a.compute_mults(np.asarray([0.,0.,0.,0.,2.,2.,3.,5.,5.,5.,5.], np.float))
+
+    assert len(mults) == 4
+    assert mults[0] == 4
+    assert mults[1] == 2
+    assert mults[2] == 1
+    assert mults[3] == 4
+
+
+def test_compute_n_dofs():
+    a = BsplineVectorSpace(3, [0.,0.,0.,0.,2.,2.,3.,5.,5.,5.,5.])
+    assert a.compute_n_dofs(np.asarray([0.,2.,3.,5.], np.float),
+                                       np.asarray([4,2,1,4], np.int_), 3) == 7
+
