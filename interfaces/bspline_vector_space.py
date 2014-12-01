18496f995d91d82a238bd1c7e61b61b1abeaa225
Marco Tezzele <marcotez@gmail.com>
2014-12-01 12:07:14 +0100

    Added compute_n_dofs and compute_mults

--- interfaces/bspline_vector_space.py
+++ interfaces/bspline_vector_space.py
@@ -10,7 +10,7 @@ import numpy as np
 
 
 class BsplineVectorSpace(object):
-    """An abstract python interface used to describe *one dimensional
+    """A python interface used to describe *one dimensional Bspline basis
     functions* on `[a,b]`, as *coefficients* times *basis functions*, with
     access to single basis functions, their derivatives, their support and
     the  splitting of `[a,b]` into sub-intervals where *each basis* is
@@ -19,19 +19,50 @@ class BsplineVectorSpace(object):
     The base class constructs the constant vector space.
     """
     def __init__(self, degree=0, knots=[0., 1.]):
-        """ Pure interface class. It generates the constant on [0,1]. """
+        """ Pure interface class. It generates the constant on [0,1] if no arguments are provided. """
         assert degree >= 0
-        assert len(knots) > 0
+        assert len(knots) > 1
+        assert knots[0] != knots[-1]
+        
         self.degree = degree
-        self.knots = np.asarray(knots, np.float)
-        self.knots_unique = np.unique(self.knots)
+        self.knots_with_rep = np.asarray(knots, np.float)
+        self.knots_unique = np.unique(self.knots_with_rep)
+        self.n_knots = self.knots_with_rep.shape[0]
+        self.mults = self.compute_mults(self.knots_with_rep)
+        self.n_dofs = self.n_knots - (self.degree + 1)
 
-        self.n_knots = self.knots.shape[0]
- 
-        # self.n_dofs = 1
+        # self.n_dofs = self.compute_n_dofs(self.knots_unique, self.mults, self.degree)
+        
         # self.n_dofs_per_end_point = 0
         # self.n_cells = 1
         # self.cells = np.array([a, b])
+
+    def compute_mults(self, knots_with_rep):
+        assert len(knots_with_rep) > 1
+        
+        j = 1
+        mults = list()
+        
+        for i in range(1, knots_with_rep.shape[0]):
+            if knots_with_rep[i] == knots_with_rep[i-1]:
+                j += 1
+            else:
+                mults.append(j)
+                j = 1
+        mults.append(j)
+
+        return np.asarray(mults, np.int_)
+
+
+    def compute_n_dofs(self, knots_unique, mults, degree):
+        assert len(knots_unique) == len(mults)
+
+        n_dofs = 0
+        for i in range(len(knots_unique)):
+            n_dofs += mults[i]
+        n_dofs -= (degree+1)
+        return n_dofs
+
 
     # def check_index(self, i):
     #     assert i< self.n_dofs, \
--- tests/test_bspline_vector_space.py
+++ tests/test_bspline_vector_space.py
@@ -3,29 +3,46 @@ from nose.tools import *
 
 def test_bspline_vector_space_default_constructor():
     a = BsplineVectorSpace()
+    
     assert a.degree == 0
-    assert a.knots[0] == 0.
-    assert a.knots[1] == 1.
+    
+    assert a.knots_with_rep[0] == 0.
+    assert a.knots_with_rep[1] == 1.
     assert a.knots_unique[0] == 0.
     assert a.knots_unique[0] == 0.
+    
     assert a.n_knots == 2
+    
+    assert a.mults[0] == 1
+    assert a.mults[1] == 1
+
+    assert a.n_dofs == 1
     
 
 def test_bspline_vector_space_general_constructor():
     a = BsplineVectorSpace(2, [0.,0.,0.,.5,1.,1.,1.])
+   
     assert a.degree == 2
-    assert a.knots[0] == 0.
-    assert a.knots[1] == 0.
-    assert a.knots[2] == 0.
-    assert a.knots[3] == 0.5
-    assert a.knots[4] == 1.
-    assert a.knots[5] == 1.
-    assert a.knots[6] == 1.
+    
+    assert a.knots_with_rep[0] == 0.
+    assert a.knots_with_rep[1] == 0.
+    assert a.knots_with_rep[2] == 0.
+    assert a.knots_with_rep[3] == 0.5
+    assert a.knots_with_rep[4] == 1.
+    assert a.knots_with_rep[5] == 1.
+    assert a.knots_with_rep[6] == 1.
     
     assert a.knots_unique[0] == 0.
     assert a.knots_unique[1] == 0.5
     assert a.knots_unique[2] == 1.
+    
     assert a.n_knots == 7
+
+    assert a.mults[0] == 3
+    assert a.mults[1] == 1
+    assert a.mults[2] == 3
+
+    assert a.n_dofs == 4
 
     # try:
     #     a.check_index(2)
