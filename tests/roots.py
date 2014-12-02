f02233bf077da109159e9f3c756c1fcb15a48221
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2014-12-02 11:20:42 -0500

    added the root functions file.

---
+++ tests/roots.py
@@ -0,0 +1,29 @@
+def root_bisec(f,a,b,error=10**-7){
+    '''Root of a function using the bisection method
+    
+    The aruments expected are:
+    f: a function with the format double f(double x)
+    a,b: bounds with a separated root.
+    error: precision for the root determination default value 10^-7
+    '''
+    ext=f(a)*f(b)
+    if ext>0:
+        return nul
+    elif ext==0:
+        for t in [a,b]:
+            if f(t)==0:
+                return t
+    else:
+        while abs(a-b)>error:
+            x=(a+b)/2
+            ext=f(a)*f(x)
+            if ext==0:
+                return x
+            elif ext>0:
+                a=x
+            else
+                b=x
+        return x
+    }
+    
+
