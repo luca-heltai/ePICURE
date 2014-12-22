004b63938c307ee73aa85b4ef30c077c7b28d604
Giuseppe Pitton <gpitton@sissa.it>
2014-12-22 11:55:26 +0100

    Legendre Vector Space

---
+++ interfaces/legendre_vector_space.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy.special
+from interfaces.lagrange_vector_space import LagrangeVectorSpace
+
+
+class LegendreVectorSpace(LagrangeVectorSpace):
+    """A python interface used to describe *one dimensional lagrange basis functions
+    on n legendre collocation points*.
+    """
+    def __init__(self, n, continuous=True):
+         LagrangeVectorSpace.__init__(self, self.collocationJacobi(0,0,n), continuous)
+
+    def collocationJacobi(self, alpha, beta, n):
+         Jroots = np.zeros(n)
+         # Jacobi polynomials are defined on (-1,1): rescale to (0,1)
+         assert (alpha == 0 and beta == 0), 'Using non-Legendre Jacobi polynomials. \
+                         Careful: weak formulation needs weights.'
+         assert (n > 2), 'Polynomial degree too low for Legendre polynomials.\
+                         Use linear Lagrange instead.'
+         Jroots[1:-1] = 0.5*(scipy.special.j_roots(n, alpha, beta)[0]+1)
+         Jroots[-1] = 1.
+         return Jroots
---
+++ tests/test_legendre_vector_space.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from interfaces.legendre_vector_space import *
+
+def test_continuous_legendre_interface():
+        vector_space = LegendreVectorSpace(8)
+        assert vector_space.n_dofs == n
+        assert vector_space.n_cells == n
+        try:
+            vector_space.check_index(n+2)
+            assert False, 'Expecting Failure!'
+        except:
+            pass
+        xi = np.linspace(0,1,n)
+        for i in xrange(n):
+            assert vector_space.basis(i,xi[i]) == 1
+
+        try:
+            vector_space.basis(0,1.2)
+            assert False, 'Expecting Failure!'
+        except:
+            pass
+
+        def plot_basis(i,n):
+                # useful for a visual check of basis functions
+                xi = np.linspace(0,1,2**8)
+                f1 = plt.figure()
+                for i in xrange(n):
+                    plt.plot(xi,[vector_space.basis(i,j) for j in xi],'-')
+                plt.plot(vector_space.collocationJacobi(0,0,n),np.zeros(n),'ro')
+                plt.show()
+
+test_continuous_legendre_interface()
+print 'all ok'
