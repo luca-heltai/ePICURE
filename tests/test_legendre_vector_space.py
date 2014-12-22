e3938b39c9043a6693f5d1f928f9fc9d299035a7
Giuseppe Pitton <gpitton@sissa.it>
2014-12-22 16:11:01 +0100

    fixed test, added discontinuous Legendre

--- tests/test_legendre_vector_space.py
+++ tests/test_legendre_vector_space.py
@@ -13,8 +13,9 @@ def test_continuous_legendre_interface():
         except:
             pass
         xi = vector_space.collocationJacobi(0,0,n)
+        eps = 1.e-11
         for i in xrange(n):
-            assert vector_space.basis(i)(xi[i]) == 1
+            assert vector_space.basis(i)(xi[i] - 1.) < eps
 
         try:
             vector_space.basis(0)(1.2)
@@ -31,5 +32,26 @@ def test_continuous_legendre_interface():
                 plt.plot(vector_space.collocationJacobi(0,0,n),np.zeros(n),'ro')
                 plt.show()
 
+def test_discontinuous_legendre_interface():
+        n = 8
+        vector_space = DiscontinuousLegendreVectorSpace(n)
+        assert vector_space.n_dofs == n
+        #assert vector_space.n_cells == n
+        try:
+            vector_space.check_index(n+2)
+            assert False, 'Expecting Failure!'
+        except:
+            pass
+        xi = vector_space.collocationJacobi(0,0,n)
+        eps = 1.e-11
+        for i in xrange(n):
+            assert vector_space.basis(i)(xi[i] - 1.) < eps
+
+        try:
+            vector_space.basis(0)(1.2)
+            assert False, 'Expecting Failure!'
+        except:
+            pass
+
 test_continuous_legendre_interface()
-print 'all ok'
+test_discontinuous_legendre_interface()
