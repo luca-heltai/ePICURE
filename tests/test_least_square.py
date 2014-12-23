b7593d95f8fda9e4d90964c87997f31b8a8afbc2
Mauro <esenonfossiio@Mauros-MBP.homenet.telecomitalia.it>
2014-12-23 12:48:48 +0100

    Vectorial test for least_square

--- tests/test_least_square.py
+++ tests/test_least_square.py
@@ -33,3 +33,25 @@ def test_least_square():
         pass
     assert works, \
            'Unexpected failure! The number of points is greater than the dofs'
+
+def vectorial_test_least_square():
+    vector_space = UniformLagrangeVectorSpace(4)
+    function=lambda t: np.array([t,t**2,t**3]).T
+    points = np.array([0, 0.3, 0.6, 0.8, 1.0])
+    works = False
+    try:
+        least_square(function, vector_space, points)
+        works = True
+    except:
+        pass
+    assert works, \
+           'Unexpected failure in the vector case!'
+
+    n=2**3+1
+    knots = np.linspace(0,1,n)
+    vector_space = IteratedVectorSpace(UniformLagrangeVectorSpace(5),knots)
+    function=lambda t: np.array([t,t**2,t**3]).T
+    points = np.linspace(0,1,2**7+1)
+    eps=10e-1
+    assert (abs(least_square(function, vector_space, points)[17][:]-np.array([.5,.25,.125]))<eps).all(),\
+            'Unexpected failure! Bad approximation'
