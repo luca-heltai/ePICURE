ac5c59e1e6b0403082738933cd4ae23a5cf5a657
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2015-01-06 18:37:36 +0100

    Corrected the test in the test folder and in the notebook.

--- tests/test_massmatix.py
+++ tests/test_massmatix.py
@@ -4,12 +4,13 @@ import numpy as np
 import matplotlib.pylab as plt
 
 def test_massmatrix():
+    print "This should takes ~8s in a core i5"
     for i in range(10):
         for j in range(2,i):
             vs=IteratedVectorSpace(UniformLagrangeVectorSpace(j), np.linspace(0,1,i))
             v=np.ones(vs.n_dofs)
             for k in range(2,10):
                 t=massmatrix(vs,k)
-                print v.dot(t.dot(v))
+                assert abs(v.dot(t.dot(v))-1.0)<10**(-10)
 
 test_massmatrix()
