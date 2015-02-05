212a6fc61ab93bab5f6e036e90a91a0bb6efc667
Nicola Giuliani <ngiuliani@sissa.it>
2015-02-05 12:11:39 +0100

    added the routine to least square some points

---
+++ utilities/least_square_by_points.py
@@ -0,0 +1,24 @@
+import numpy as np
+from interfaces.vector_space import *
+from interfaces.lagrange_vector_space import *
+from utilities.matrices import *
+from numpy.linalg import lstsq
+
+def least_square_by_points(vs, points, q):
+    """Returns the array c of the coefficients of the least square
+    approximation of the points provided.
+    """
+    n = vs.n_dofs
+    m = len(points)
+    print len(q), len(points), points.shape
+    assert len(q) == m, \
+           'The number of points must be the same as the provided parameter subdivision'
+
+    assert len(q) >= n, \
+           'The number of points must be greater or equal to the number of dofs'
+    
+    M = interpolation_matrix(vs, q)
+    b = points
+    c = lstsq(M, b)[0]
+
+    return c
