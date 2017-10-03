297cb3adbd820f12fa604e815adf1b8e73cccf02
Nicola Giuliani <ngiuliani@sissa.it>
2017-10-03 15:37:42 +0200

    minor bug

--- interfaces/vector_space.py
+++ interfaces/vector_space.py
@@ -68,7 +68,7 @@ class VectorSpace(object):
         y = np.zeros(sy)
         # TBD: make this function aware of locality
         for i in xrange(self.n_dofs):
-            y += np.outer(c[i], self.basis(i)(x)).reshape(sy)
+            y = y + np.outer(c[i], self.basis(i)(x)).reshape(sy)
         return y
     
     def eval_der(self, c, d, x):
